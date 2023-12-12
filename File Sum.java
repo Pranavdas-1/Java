8631cee022d3099fba98d761e120f9262c90f9f2
Pranav Das <138027582+Pranavdas-1@users.noreply.github.com>
2023-12-12 15:14:18 +0530

    Create File Sum.java

---
+++ File Sum.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+-/Desktop/pranav
+
+import java.util.*;
+
+class file{
+
+public static void main(String args[]) throws IOException{
+
+byte a[]=new byte[20];
+
+Am I int?
+
+FileInputStream f=new FileInputStream("helo.txt");
+
+f.read(a);
+
+String s=new String(a);
+
+StringTokenizer sc=new StringTokenizer(s); {
+
+while(sc.hasMoreTokens)
+
+num = Integer.parseInt(sc.nextToken()); System.out.print(number);
+
+am+=whether;
+
+}
+
+System.out.print("sum is"+sum);
+
+}
+
+A}
